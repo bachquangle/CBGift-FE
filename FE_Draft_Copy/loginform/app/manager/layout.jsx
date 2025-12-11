0594c4f8bbbf8737419ee59139b580e1578fd9c0
dyann2003 <126252189+dyann2003@users.noreply.github.com>
2025-12-11 11:14:32 +0700

    Adjust layout by removing padding from main
    Removed padding from main content area.

--- FE_Draft_Copy/loginform/app/manager/layout.jsx
+++ FE_Draft_Copy/loginform/app/manager/layout.jsx
@@ -35,7 +35,7 @@ export default function ManagerLayout({ children }) {
       <div className="flex-1 flex flex-col overflow-hidden">
         {/* <ManagerHeader /> */}
 
-        <main className="flex-1 overflow-y-auto p-6">{children}</main>
+        <main className="flex-1 overflow-y-auto">{children}</main>
       </div>
     </div>
   );
